4836d3e5bdf31d53ad96786d80382c3a9056faff
Suryasrikar12 <suryasrikar12@gmail.com>
2026-05-07 20:12:56 +0530

    fix secrets for streamlit cloud

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 # ---- Page Config ----
 st.set_page_config(page_title="AI Resume Analyzer", page_icon="📄")
